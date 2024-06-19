f75714f23d5ef6c6f495c1a8dd27ae34bb63ac6a
izoi <irene.zoi@cern.ch>
2024-06-19 09:52:27 -0500

    bug fix

--- generation/newGiacomoWorkflow/2017/submissionMaker_allsteps.py
+++ generation/newGiacomoWorkflow/2017/submissionMaker_allsteps.py
@@ -1,17 +1,17 @@
 import os
 import fileinput
 
-submitNano = True
+
 year    = "2017"
 samples = [
-"aQGC_WMhadZlepJJ_EWK_LO_SM_mjj100_pTj10",
+# "aQGC_WMhadZlepJJ_EWK_LO_SM_mjj100_pTj10",
 # "aQGC_WPlepWMhadJJ_EWK_LO_SM_mjj100_pTj10",
-# "aQGC_ZlepZhadJJ_EWK_LO_SM_mjj100_pTj10",
+"aQGC_ZlepZhadJJ_EWK_LO_SM_mjj100_pTj10",
 # "aQGC_WPhadZlepJJ_EWK_LO_SM_mjj100_pTj10",
 # "aQGC_WPhadWMlepJJ_EWK_LO_SM_mjj100_pTj10",
 # "aQGC_WMlepZhadJJ_EWK_LO_SM_mjj100_pTj10",
 # "aQGC_WPlepZhadJJ_EWK_LO_SM_mjj100_pTj10",
-# # "aQGC_WMlepWMhadJJ_EWK_LO_SM_mjj100_pTj10",
+# "aQGC_WMlepWMhadJJ_EWK_LO_SM_mjj100_pTj10",
 # "aQGC_WPlepWPhadJJ_EWK_LO_SM_mjj100_pTj10"
 ]
 
@@ -29,15 +29,8 @@ for sample in samples:
     os.system("mkdir /eos/uscms/store/group/lnujj/aQGC_VVJJ_Private_Production_2017_GiacomoChain/{}/nAOD".format(sample))
     
     
-    # submit pLHE
-    wrapperFile = 'wrapper_pLHE_fullproduction.sh'
-    # jdlFile     = 'submit_pLHE_fullproduction.jdl'
-    if submitNano == True:
-        print(" Submitting Nano!!!")
-        # submit nano
-        wrapperFile = 'wrapper_nAOD_fullproduction.sh'
-        # jdlFile     = 'submit_nAOD_fullproduction.jdl'
 
+    wrapperFile = 'wrapper_pLHE_fullproduction.sh'
     wrapperFile_new = '{}'.format(wrapperFile)
     filein = open('{}'.format(wrapperFile))
     fileout = open('{}{}'.format(directory,wrapperFile_new),"wt")
@@ -45,7 +38,15 @@ for sample in samples:
         fileout.write(line.replace('{SAMPLE}',sample))
     filein.close()
     fileout.close()
-
+    
+    wrapperFile = 'wrapper_nAOD_fullproduction.sh'
+    wrapperFile_new = '{}'.format(wrapperFile)
+    filein = open('{}'.format(wrapperFile))
+    fileout = open('{}{}'.format(directory,wrapperFile_new),"wt")
+    for line in filein:
+        fileout.write(line.replace('{SAMPLE}',sample))
+    filein.close()
+    fileout.close()
 
     jdlFile="submit_allsteps.jdl"
     jdlFile_new = '{}'.format(jdlFile)
